2679e0f071c13e445d14b1a998a06a56be3b0140
Srinivasa Balaji <Balaji.Srinivasa@volvo.com>
2023-09-18 16:55:05 +0200

    Spring Doc - API

--- src/main/java/com/student/system/StudentSystemApplication.java
+++ src/main/java/com/student/system/StudentSystemApplication.java
@@ -3,10 +3,6 @@ package com.student.system;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Slf4j
 @SpringBootApplication
@@ -14,12 +10,6 @@ public class StudentSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentSystemApplication.class, args);
-    }
-
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.student.system")).build();
     }
 
 }
